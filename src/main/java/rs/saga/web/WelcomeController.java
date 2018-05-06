c06167bf79fef8198696ae39f92b4afa8a1b63fb
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-05-06 20:45:29 +0200

    external server

---
+++ src/main/java/rs/saga/web/WelcomeController.java
@@ -0,0 +1,13 @@
+package rs.saga.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "hello";
+    }
+}
