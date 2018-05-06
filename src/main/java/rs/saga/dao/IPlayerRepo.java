063b0a4748e17dab7e1ede86728f966591de4918
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-05-06 10:23:29 +0200

    spring-boot-01

--- src/main/java/rs/saga/dao/IPlayerRepo.java
+++ src/main/java/rs/saga/dao/IPlayerRepo.java
@@ -2,11 +2,8 @@ package rs.saga.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import rs.saga.domain.Player;
 
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -16,9 +13,4 @@ import java.util.Set;
 public interface IPlayerRepo extends JpaRepository<Player, Long> {
 
     Set<Player> findByFirstName(String firstName);
-
-    Player findByCredentials_Username(String username);
-
-    @Query("select r.roleName from Role r left join r.player p where  p.credentials.username = :userName")
-    List<String> findPlayerRoles(@Param("userName") String userName);
 }
--- src/main/java/rs/saga/init/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package rs.saga.init;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
